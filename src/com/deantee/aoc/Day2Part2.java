008fd76a37008ad7e8b4ff0653c2af4ddcf87892
Dean <64692815+DigitalDean06@users.noreply.github.com>
2021-12-14 00:19:33 +0800

    Add solution of day 2 part 2

---
+++ src/com/deantee/aoc/Day2Part2.java
@@ -0,0 +1,33 @@
+package com.deantee.aoc;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Day2Part2 {
+
+  public static void main(String[] args) {
+    int horizontal = 0;
+    int depth = 0;
+    int aim = 0;
+    try {
+      for (String line : Files.readAllLines(Paths.get("inputs", "day-2.txt"))) {
+        String[] parameters = line.split(" ");
+        String action = parameters[0];
+        int value = Integer.parseInt(parameters[1]);
+        switch (action) {
+          case "forward" -> {
+            horizontal += value;
+            depth += aim * value;
+          }
+          case "down" -> aim += value;
+          case "up" -> aim -= value;
+          default -> throw new IllegalArgumentException("Invalid action");
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    System.out.println(horizontal * depth);
+  }
+}
