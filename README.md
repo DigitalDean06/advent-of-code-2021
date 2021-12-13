497a7477d0f728721d3cc3680d4c7614d2ca68dd
Dean <64692815+DigitalDean06@users.noreply.github.com>
2021-12-13 21:48:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2021
+
+A repository of my solutions for Advent of Code 2021
---
+++ src/com/deantee/aoc/Day1.java
@@ -0,0 +1,22 @@
+package com.deantee.aoc;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Day1 {
+
+  public static void main(String[] args) {
+    Integer last = null;
+    int n = 0;
+    try {
+      for (int i : Files.readAllLines(Paths.get("inputs", "day-1.txt")).stream().map(Integer::parseInt).toList()) {
+        if (last != null && last < i) ++n;
+        last = i;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    System.out.println(n);
+  }
+}
